01ef05bb8049b9728592b2ff77d17b05a38db472
Rekha Bhagavatula <rekha.bhagavatula@sofaworks.co.uk>
2017-09-15 08:11:17 +0100

    WA-2 some more for promises

--- app/examplepromise.js
+++ app/examplepromise.js
@@ -13,12 +13,32 @@ getTempCallback('Manchester', function (err, temp) {
 
 function getTempPromise (location) {
   return new Promise( function(resolve, reject) {
-    resolve(50);
-    reject('city not found');
+    setTimeout(function() {
+      resolve(50);
+      reject('city not found');
+    }, 2000);
+
   });
 }
 getTempPromise('Manchester').then(function(temp) {
   console.log('Pro sucess', temp);
 }, function(err) {
   console.log('Promise err', err);
+})
+
+
+function addPromise (a,b) {
+  return new Promise ( function (resolve, reject) {
+    if(typeof a === 'number' && typeof b === 'number') {
+      resolve(a+b);
+    } else {
+      reject ('Not valid values');
+    }
+  });
+}
+
+addPromise(7,1).then(function(value) {
+  console.log(value);
+}, function(err) {
+  console.log('Error', err);
 })
