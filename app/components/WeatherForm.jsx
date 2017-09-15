a2a9adb661f0390f348d4597c9cc526925af47d1
Rekha Bhagavatula <rekha.bhagavatula@laterooms.com>
2017-09-15 15:58:44 +0100

    WA-3 just added onformsubmit function

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const WeatherForm = React.createClass({
+  onFormSubmit: function () {
+
+  }
   render: function() {
     return(
       <div>
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <input type="text"/>
           <button>Get Weather</button>
         </form>
