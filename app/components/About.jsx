9bf64dfe5ec2d8d03845beaef3fdf3d974e0320d
Rekha Bhagavatula <rekha.bhagavatula@sofaworks.co.uk>
2018-02-18 21:02:56 +0000

    WA-9 styled about us page added some blurb

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,23 @@ import React from 'react';
 const About =  (props)  => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a Weather app built in react. I have built this app to practice react
+      </p>
+      <p>
+        Here are some of the tools i used.
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> -
+            This is the JS framework used
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open weather Map</a> -
+            To search for weather data by city name api from here.
+          </li>
+        </ul>
+      </p>
     </div>
   );
 }
