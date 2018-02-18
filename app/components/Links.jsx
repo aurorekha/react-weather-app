d2b8feed8172aa33e4ca38655b63da202358d870
Rekha Bhagavatula <rekha.bhagavatula@sofaworks.co.uk>
2018-02-18 20:35:40 +0000

    WA-8 added nav styles

--- app/components/Links.jsx
+++ app/components/Links.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import {NavLink, IndexLink, Link} from 'react-router-dom';
 
-const Links = React.createClass({
-  render: function () {
+class Links extends React.Component{
+  onSearch(e) {
+      e.preventDefault();
+      alert('Not wired up!')
+  }
+  render() {
     return (
-        <div>
-        <NavLink
-          exact
-          to="/"
-          activeClassName="active"
-          activeStyle={{fontWeight: 'bold', color: 'red'}}>
-          Get Weather
-        </NavLink>
-        <NavLink
-          to="/about"
-          activeClassName="active"
-          activeStyle={{fontWeight: 'bold', color: 'red'}}>
-          About
-        </NavLink>
-        <NavLink
-          to="/examples"
-          activeClassName="active"
-          activeStyle={{fontWeight: 'bold', color: 'red'}}>
-          Examples
-        </NavLink>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                activeStyle={{fontWeight: 'bold'}}>
+                Get Weather
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                activeClassName="active"
+                activeStyle={{fontWeight: 'bold'}}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/examples"
+                activeClassName="active"
+                activeStyle={{fontWeight: 'bold'}}>
+                Examples
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit"  className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     )
   }
-});
+};
 
 export default Links;
